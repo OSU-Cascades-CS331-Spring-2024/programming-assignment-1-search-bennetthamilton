65611a661ebb2e9a5637efcc50402be8be3b1ccc
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-22 15:56:41 -0700

    added todos

--- search.py
+++ search.py
@@ -1,5 +1,8 @@
 # Description: Search algorithm classes to represent each algorithm (breadth-first search, 
 #              iterative deepening depth-limited search, uniform-cost search, and A* search)
+
+from collections import deque
+
 class SearchAlgorithm:
     def __init__(self, problem):
         self.problem = problem
@@ -10,16 +13,20 @@ class SearchAlgorithm:
 # Subclasses: BFS, IDS, UCS, A*
 class BreadthFirstSearch(SearchAlgorithm):
     def search(self):
+        # todo: implement breadth-first search
         pass
 
 class IterativeDLS(SearchAlgorithm):
     def search(self):
+        # todo: implement iterative depth-limited search
         pass
 
 class UniformCostSearch(SearchAlgorithm):
     def search(self):
+        # todo: implement uniform-cost search
         pass
 
 class AStarSearch(SearchAlgorithm):
     def search(self):
+        # todo: implement A* search
         pass
