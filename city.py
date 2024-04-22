9ebc7c1ddaf273938dc9ec53843117358e368636
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-22 14:55:20 -0700

    added description

--- city.py
+++ city.py
@@ -1,4 +1,4 @@
-
+# Description: City class which is used to represent a city with a name (str) and coordinates (tuple of floats).
 class City:
     def __init(self, name, coordinates):
         self.name = name
