266b662eaac4482a9cd18b3bb4ab1e502a8702d8
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-22 14:50:29 -0700

    added method for adding connections

--- map.py
+++ map.py
@@ -2,7 +2,12 @@
 class Map:
     def __init__(self):
         self.cities = {}
-        self.roads = {}
+        self.connections = {}
 
     def add_city(self, city):
         self.cities[city.name] = city
+
+    def add_connection(self, city1, city2, distance):
+        if city1 not in self.roads:
+            self.roads[city1] = {}
+        self.roads[city1][city2] = distance
