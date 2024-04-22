1842d7057879d83365b1fff0ba7764a5332a57ff
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-22 16:15:38 -0700

    fixed bug when passing map data to run function, reverted search algo constant back to array of strings

--- main.py
+++ main.py
@@ -11,15 +11,11 @@ import sys
 import argparse
 from map import Map
 from city import City
-from search import SearchAlgorithm, BreadthFirstSearch, IterativeDLS, UniformCostSearch, AStarSearch
+from search import SearchAlgorithm
 
 # define constants
-SEARCH_ALGOS = {
-    "bfs": BreadthFirstSearch,
-    "dls": IterativeDLS,
-    "ucs": UniformCostSearch,
-    "astar": AStarSearch
-}
+SEARCH_ALGOS = ["bfs", "dls", "ucs", "astar"]
+    
 
 # function to parse command line arguments
 # ref: https://docs.python.org/3/library/argparse.html
@@ -34,7 +30,7 @@ def parse_args():
     parser.add_argument("map_file", help="Text file containing map data")
     parser.add_argument("-A", "--start_city", help="Starting city")
     parser.add_argument("-B", "--end_city", help="Ending city")
-    parser.add_argument("-S", "--search_algorithm", choices=SEARCH_ALGOS.keys(), default="bfs", help="Search algorithm to use (default: BFS)")
+    parser.add_argument("-S", "--search_algorithm", choices=SEARCH_ALGOS, default=SEARCH_ALGOS[0], help="Search algorithm to use (default: BFS)")
 
     print("Parsing command line arguments...")
 
@@ -75,14 +71,14 @@ def read_map_data(map_file):
     return map_data
 
 # function to run search algorithm
-def run_search(map_file, start_city, end_city, search_algorithm):
-    # TODO
+def run_search(map, start_city, end_city, search_algorithm):
+    # create search algorithm object
     pass
 
 # function to run all search algorithms
-def run_all_searches(map_file, start_city, end_city):
-    for algorithm in SEARCH_ALGOS.keys():
-        run_search(map_file, start_city, end_city, algorithm)
+def run_all_searches(map, start_city, end_city):
+    for algorithm in SEARCH_ALGOS:
+        run_search(map, start_city, end_city, algorithm)
 
 # main function to run program
 def main():
@@ -91,7 +87,7 @@ def main():
     map_file = args.map_file
 
     # Read map data from file
-    map_date = read_map_data(map_file)
+    map_data = read_map_data(map_file)
 
     # if start and end are not provided, use predefined city pairs
     if not (args.start and args.goal):
@@ -108,12 +104,12 @@ def main():
         ]
         for start, goal in city_pairs:
             # Perform search for given city pairs using all search algorithm
-            run_all_searches(map_file, start, goal)
+            run_all_searches(map_data, start, goal)
             # Write results to solutions.txt
         pass
     else:
         # Perform search for given start and goal cities using specified search algorithm
-        run_search(map_file, args.start_city, args.end_city, args.search_algorithm)
+        run_search(map_data, args.start_city, args.end_city, args.search_algorithm)
         # Write results to solutions.txt
         pass
 
