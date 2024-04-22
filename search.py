78c279215300523c38c765d26d165468afc78533
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-22 16:14:09 -0700

    added static method for creating new instance of search algorithm based on name

--- search.py
+++ search.py
@@ -4,6 +4,20 @@
 from collections import deque
 
 class SearchAlgorithm:
+    # ref: https://www.digitalocean.com/community/tutorials/python-static-method
+    @staticmethod
+    def from_name(name, map_data, start_city, end_city):
+        if name == "bfs":
+            return BreadthFirstSearch(map_data, start_city, end_city)
+        elif name == "dls":
+            return IterativeDLS(map_data, start_city, end_city)
+        elif name == "ucs":
+            return UniformCostSearch(map_data, start_city, end_city)
+        elif name == "astar":
+            return AStarSearch(map_data, start_city, end_city)
+        else:
+            raise ValueError("Unknown search algorithm: {}".format(name))
+        
     def __init__(self, map_data, start_city, end_city):
         self.map_data = map_data
         self.start_city = start_city
