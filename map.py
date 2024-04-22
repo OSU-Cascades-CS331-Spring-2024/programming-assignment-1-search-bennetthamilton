177343e29ff9d27dfcc5acf3d6c5c70a5806d504
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-22 13:46:35 -0700

    more detailed descriptions, implemented all methods

--- map.py
+++ map.py
@@ -5,9 +5,32 @@ class Map:
         self.roads = {}
 
     def add_city(self, city_name, coordinates):
-        # add city to map
-        pass
+        """
+        add city to map
+
+        params:
+            city_name (str): name of city
+            coordinates (tuple): coordinates of city
+
+        return none
+        """
+        self.cities[city_name] = coordinates
 
     def add_road(self, city1, city2, distance):
-        # add road to map
-        pass
+        """
+        add road to map
+
+        params:
+            city1 (str): name of city 1
+            city2 (str): name of city 2
+            distance (int): distance between city 1 and city 2
+
+        return none
+        """
+        if city1 not in self.roads:
+            self.roads[city1] = {}
+        self.roads[city1][city2] = distance
+
+        if city2 not in self.roads:
+            self.roads[city2] = {}
+        self.roads[city2][city1] = distance
