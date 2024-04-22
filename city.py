c76ae39cb8e8dabdc4876ce805b3f5f386eed1af
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-22 15:00:07 -0700

    fixed typo

--- city.py
+++ city.py
@@ -1,5 +1,5 @@
 # Description: City class which is used to represent a city with a name (str) and coordinates (tuple of floats).
 class City:
-    def __init(self, name, coordinates):
+    def __init__(self, name, coordinates):
         self.name = name
         self.coordinates = coordinates
