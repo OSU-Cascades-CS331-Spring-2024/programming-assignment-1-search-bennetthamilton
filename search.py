a4c2b987fb145039d6e84093f143eff73d7f7b4e
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-22 17:25:26 -0700

    added more data to store

--- search.py
+++ search.py
@@ -1,8 +1,6 @@
 # Description: Search algorithm classes to represent each algorithm (breadth-first search, 
 #              iterative deepening depth-limited search, uniform-cost search, and A* search)
 
-from collections import deque
-
 class SearchAlgorithm:
     # ref: https://www.digitalocean.com/community/tutorials/python-static-method
     @staticmethod
@@ -17,17 +15,25 @@ class SearchAlgorithm:
             return AStarSearch(map_data, start_city, end_city)
         else:
             raise ValueError("Unknown search algorithm: {}".format(name))
-        
+    
     def __init__(self, map_data, start_city, end_city):
         self.map_data = map_data
         self.start_city = start_city
         self.end_city = end_city
+        self.path = []
+        self.cost = 0
+        self.frontier = []          # the queue of nodes to be expanded
+        self.explored = set()       # the set of nodes that have been expanded
+        self.nodes_explored = 0     # the number of nodes removed from the frontier
+        self.nodes_expanded = 0     # the total number of successors
+        self.nodes_maintained = 0   # the number of nodes stored in the frontier
 
     def search(self):
         raise NotImplementedError
     
 # Subclasses: BFS, IDS, UCS, A*
 class BreadthFirstSearch(SearchAlgorithm):
+    # ref: https://www.geeksforgeeks.org/python-program-for-breadth-first-search-or-bfs-for-a-graph/
     def search(self):
         # todo: implement breadth-first search
         pass
