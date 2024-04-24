aa4229974a11c4aa75f28e7def1af9c4a8974ba3
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-23 18:33:37 -0700

    implemented bfs

--- search.py
+++ search.py
@@ -1,6 +1,9 @@
 # Description: Search algorithm classes to represent each algorithm (breadth-first search, 
 #              iterative deepening depth-limited search, uniform-cost search, and A* search)
 
+# ref: https://docs.python.org/3/library/queue.html
+import queue
+
 class SearchAlgorithm:
     # ref: https://www.digitalocean.com/community/tutorials/python-static-method
     @staticmethod
@@ -33,22 +36,53 @@ class SearchAlgorithm:
     
 # Subclasses: BFS, IDS, UCS, A*
 class BreadthFirstSearch(SearchAlgorithm):
+    def __init__(self, map_data, start_city, end_city):
+        super().__init__(map_data, start_city, end_city)
+        self.parents = {}
+
     # ref: https://www.geeksforgeeks.org/python-program-for-breadth-first-search-or-bfs-for-a-graph/
     def search(self):
-        # todo: implement breadth-first search
-        pass
+        q = queue.Queue()
+        q.put(self.start_city)
+        self.frontier.append(self.start_city)
+        self.explored.add(self.start_city)
+        self.parents = {self.start_city: None}
+        
+        while not q.empty():
+            current_city = q.get()
+            
+            if current_city == self.end_city:
+                break
+            
+            for city in self.map_data.connections[current_city]:
+                if city not in self.explored:
+                    q.put(city)
+                    self.frontier.append(city)
+                    self.explored.add(city)
+                    self.parents[city] = current_city
+
+    def get_path(self):
+        # Reconstruct path from end city to start city
+        current = self.end_city
+        while current is not None:
+            self.path.append(current)
+            current = self.parents[current]
+        self.path.reverse()
 
 class IterativeDLS(SearchAlgorithm):
     def search(self):
         # todo: implement iterative depth-limited search
+
         pass
 
 class UniformCostSearch(SearchAlgorithm):
     def search(self):
         # todo: implement uniform-cost search
+
         pass
 
 class AStarSearch(SearchAlgorithm):
     def search(self):
         # todo: implement A* search
+
         pass
