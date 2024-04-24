0ca38aa2cff6f02a26ac7812d24704bcb1091567
bennetthamilton <bennetthamilton0@gmail.com>
2024-04-23 18:55:31 -0700

    implemented functions for computing cost

--- map.py
+++ map.py
@@ -11,3 +11,12 @@ class Map:
         if city1 not in self.connections:
             self.connections[city1] = {}
         self.connections[city1][city2] = distance
+
+    def get_connection_distance(self, city1, city2):
+        return self.connections[city1][city2]
+    
+    def compute_path_distance(self, path):
+        total_distance = 0
+        for i in range(len(path) - 1):
+            total_distance += self.get_connection_distance(path[i], path[i+1])
+        return total_distance
